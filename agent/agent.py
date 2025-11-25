8d55002ddef36e2d56573c9256e25bf9bb82c196
Piyush Choudhari <choudhari.piyush@gmail.com>
2025-11-25 06:34:25 +0000

    update description

--- agent/agent.py
+++ agent/agent.py
@@ -3,5 +3,5 @@ from google.adk.agents.llm_agent import LlmAgent
 capital_agent = LlmAgent(
     model="gemini-2.5-flash",
     name="peer_review_agent",
-    description="Answers user questions about the capital city of a given country.",
+    description="An Expert Peer Reviewer, a highly experienced editor and fact-checker for professional technical and non-technical blog posts.",
 )
